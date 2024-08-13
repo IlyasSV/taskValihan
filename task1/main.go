0403770696c9bc4a3a7b350f1b63d482e8516965
Ilyas <bdkydrov@gmail.com>
2024-08-13 17:27:14 +0300

    Внес изменения в task1\main.go в поле Address

--- task1/main.go
+++ task1/main.go
@@ -10,11 +10,17 @@ import (
 
 func main() {
 	emp := []model.Employee{
-		{Person: model.Person{Name: "11", Age: 20, Address: "111"},
+		{Person: model.Person{Name: "11", Age: 20, Address: model.Address{
+			City: "1414", Street: "23546", PostalCode: 1245,
+		}},
 			Position: 1},
-		{Person: model.Person{Name: "22", Age: 21, Address: "222"},
+		{Person: model.Person{Name: "22", Age: 21, Address: model.Address{
+			City: "1414", Street: "23546", PostalCode: 1245,
+		}},
 			Position: 2},
-		{Person: model.Person{Name: "33", Age: 22, Address: "333"},
+		{Person: model.Person{Name: "33", Age: 22, Address: model.Address{
+			City: "1414", Street: "23546", PostalCode: 1245,
+		}},
 			Position: 3},
 	}
 	fmt.Printf("%+v\n", emp)
